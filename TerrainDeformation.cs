c7233d0959ebe3474030c720eaefb67004d184d7
Dorian Stoll <dorian@tmsp.ga>
2016-05-15 15:35:13 +0200

    Serialization

--- TerrainDeformation.cs
+++ TerrainDeformation.cs
@@ -4,7 +4,7 @@
  * Copyright (c) Thomas P. 2016
  * Licensed under the terms of the MIT License
  */
- /*
+
 using System;
 using System.Linq;
 
@@ -16,52 +16,47 @@ namespace KerbalTerrainSystem
     [KSPScenario(ScenarioCreationOptions.AddToAllGames, GameScenes.SPACECENTER, GameScenes.FLIGHT, GameScenes.EDITOR, GameScenes.TRACKSTATION)]
     public class TerrainDeformation : ScenarioModule
     {
-        /// Load the saved Deformations
+        // Load the saved Deformations
         public override void OnLoad(ConfigNode node)
         {
-            /// Loop through all child-nodes
+            // Loop through all child-nodes
             foreach (ConfigNode bodyNode in node.nodes)
             {
-                /// Get the CelestialBody
+                // Get the CelestialBody
                 CelestialBody body = PSystemManager.Instance.localBodies.Find(b => b.transform.name == bodyNode.name);
 
-                /// Get the Deformation controller and clear it
+                // Get the Deformation controller and clear it
                 PQSMod_TerrainDeformation pqsDeformation = body.pqsController.GetComponentInChildren<PQSMod_TerrainDeformation>();
                 pqsDeformation.deformations.Clear();
 
-                /// Build the Deformations
+                // Build the Deformations
                 foreach (ConfigNode deformationNode in bodyNode.nodes)
                 {
-                    Vector3d position = ConfigNode.ParseVector3D(deformationNode.GetValue("position"));
-                    Vector3d velocity = ConfigNode.ParseVector3D(deformationNode.GetValue("velocity"));
-                    Double force = Double.Parse(deformationNode.GetValue("force"));
-                    Deformation deformation = new Deformation(position, body, velocity, force);
+                    Deformation deformation = ConfigNode.CreateObjectFromConfig<Deformation>(deformationNode);
                     pqsDeformation.deformations.Add(deformation);
                 }
             }
         }
 
-        /// Save the Deformation
+        // Save the Deformation
         public override void OnSave(ConfigNode node)
         {
-            /// Loop through all bodies with a PQS
+            // Loop through all bodies with a PQS
             foreach (CelestialBody body in PSystemManager.Instance.localBodies.Where(b => b.pqsController != null))
             {
-                /// Get the Deformation controller
+                // Get the Deformation controller
                 PQSMod_TerrainDeformation pqsDeformation = body.pqsController.GetComponentInChildren<PQSMod_TerrainDeformation>();
 
-                /// Add a node for the body
+                // Add a node for the body
                 ConfigNode bodyNode = node.AddNode(body.transform.name);
 
-                /// Build the Deformations
+                // Build the Deformations
                 foreach (Deformation deformation in pqsDeformation.deformations)
                 {
                     ConfigNode deformationNode = bodyNode.AddNode("Deformation");
-                    deformationNode.AddValue("position", ConfigNode.WriteVector(deformation.position));
-                    deformationNode.AddValue("velocity", ConfigNode.WriteVector(deformation.velocity));
-                    deformationNode.AddValue("force", deformation.force);
+                    ConfigNode.CreateConfigFromObject(deformation, deformationNode);
                 }
             }
         }
     }
-}*/
+}
